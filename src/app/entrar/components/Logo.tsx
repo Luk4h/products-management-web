c775e4d285e22170a936735609d2330e7435e3d7
Luiz Gustavo Marcondes <luiz@lgvm.dev>
2023-09-05 14:03:38 +0000

    ⚡️ Adicionado priority nas imagens

--- src/app/entrar/components/Logo.tsx
+++ src/app/entrar/components/Logo.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 import logo from '@/public/logo.jpeg';
 
-const Logo = () => <Image src={logo} className='self-center' height={256} width={256} alt='Logo do grupo Tecadi'/>
+const Logo = () => <Image src={logo} className='self-center' height={256} width={256} alt='Logo do grupo Tecadi' priority={true} />
 
 export default Logo;
--- src/app/entrar/components/Splash.tsx
+++ src/app/entrar/components/Splash.tsx
@@ -4,7 +4,7 @@ import splash from "@/public/splash-screen.jpeg"
 
 const Splash = () => (
   <div className='relative w-full flex-grow md:min-h-screen flex items-end'>
-    <Image src={splash} fill alt='Centro de distribuição da Tecadi.' className='relative w-full h-full object-cover z-0'/>
+    <Image src={splash} fill alt='Centro de distribuição da Tecadi.' className='relative w-full h-full object-cover z-0' priority={true}/>
     <div className="block md:hidden w-full h-6 rounded-t-2xl shadow-xl bg-white z-10" />
   </div>
 );
